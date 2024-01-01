25d06599def8327bd51c29e76a701515bf537c20
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2024-01-02 01:03:58 +0900

    추가된 장소 목록

--- app/src/main/java/com/example/people_here/MakingTour/MakingTourAddListAdapter.kt
+++ app/src/main/java/com/example/people_here/MakingTour/MakingTourAddListAdapter.kt
@@ -4,30 +4,44 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.people_here.Data.MakingTourAddListData
+import com.example.people_here.R
 import com.example.people_here.databinding.ItemMakingTourAddListPlace1Binding
 import com.example.people_here.databinding.ItemMakingTourAddListPlace2Binding
 import com.example.people_here.databinding.ItemMakingTourAddListPlace3Binding
+import java.util.Collections
 
 class MakingTourAddListAdapter(private val addListData: ArrayList<MakingTourAddListData>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    var editSequence = true
+
+    private var isEditMode : Boolean = false
+
     companion object {
         const val TYPE_FIRST = 0
         const val TYPE_SECOND = 1
         const val TYPE_LAST = 2
     }
 
+    fun setEditMode(editMode: Boolean) {
+        isEditMode = editMode
+        notifyDataSetChanged()
+    }
+
+    fun onItemMove(fromPosition: Int, toPosition: Int) {
+        if (fromPosition < toPosition) {
+            for (i in fromPosition until toPosition) {
+                Collections.swap(addListData, i, i + 1)
+            }
+        } else {
+            for (i in fromPosition downTo toPosition + 1) {
+                Collections.swap(addListData, i, i - 1)
+            }
+        }
+        notifyItemMoved(fromPosition, toPosition)
+    }
+
     override fun getItemViewType(position: Int): Int {
         return addListData[position].itemType
     }
 
-    fun toggleItemViewType() {
-        addListData.forEach {
-            if (it.itemType == TYPE_SECOND) {
-                it.ha = !it.isHamburgurVisible
-            }
-        }
-        notifyItemRangeChanged(0, itemCount)
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when (viewType) {
@@ -43,15 +57,15 @@ class MakingTourAddListAdapter(private val addListData: ArrayList<MakingTourAddL
                 val binding = ItemMakingTourAddListPlace3Binding.inflate(LayoutInflater.from(parent.context), parent, false)
                 LastViewHolder(binding)
             }
-            else -> throw IllegalArgumentException("Invalid view type")
+            else -> throw IllegalArgumentException("이외의 viewType")
         }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val item = addListData[position]
         when (holder) {
-            is FirstViewHolder -> holder.bind(item)
-            is SecondViewHolder -> holder.bind(item)
+            is FirstViewHolder -> holder.bind(item, isEditMode)
+            is SecondViewHolder -> holder.bind(item, isEditMode)
             is LastViewHolder -> holder.bind(item)
         }
     }
@@ -59,24 +73,38 @@ class MakingTourAddListAdapter(private val addListData: ArrayList<MakingTourAddL
     override fun getItemCount(): Int = addListData.size
 
     inner class FirstViewHolder(private val binding: ItemMakingTourAddListPlace1Binding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(placeInfo: MakingTourAddListData) {
+        fun bind(placeInfo: MakingTourAddListData, isEditMode : Boolean) {
             binding.ivMakingTourAddListPlace.setImageResource(placeInfo.placeImage)
             binding.tvMakingTourAddListPlaceName.text = placeInfo.placeName
             binding.tvMakingTourListPlaceNumber.text = placeInfo.placeNumber.toString()
+
+            if (isEditMode) {
+                binding.ivMakingTourAddListPlaceDots.setImageResource(R.drawable.making_tour_add_list_hamburger) // 햄버거 드래그
+            } else {
+                binding.ivMakingTourAddListPlaceDots.setImageResource(R.drawable.ic_making_tour_add_list_dots)  // dots
+            }
+
         }
     }
 
     inner class SecondViewHolder(private val binding: ItemMakingTourAddListPlace2Binding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(placeInfo: MakingTourAddListData) {
+        fun bind(placeInfo: MakingTourAddListData, isEditMode : Boolean) {
             binding.ivMakingTourAddListPlace.setImageResource(placeInfo.placeImage)
             binding.tvMakingTourAddListPlaceName.text = placeInfo.placeName
             binding.tvMakingTourListPlaceNumber.text = placeInfo.placeNumber.toString()
+
+            if (isEditMode) {
+                binding.ivMakingTourAddListPlaceDots.setImageResource(R.drawable.making_tour_add_list_hamburger) // 햄버거 드래그
+            } else {
+                binding.ivMakingTourAddListPlaceDots.setImageResource(R.drawable.ic_making_tour_add_list_dots)  // dots
+            }
         }
+
+
     }
 
     inner class LastViewHolder(private val binding: ItemMakingTourAddListPlace3Binding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(placeInfo: MakingTourAddListData) {
-            // '장소 추가' 아이템 바인딩 로직
             binding.clMakingTourAddListPlacePlus.setOnClickListener {
                 showDialog()
             }
--- app/src/main/java/com/example/people_here/MakingTour/MakingTourAddListFragment.kt
+++ app/src/main/java/com/example/people_here/MakingTour/MakingTourAddListFragment.kt
@@ -1,21 +1,29 @@
 package com.example.people_here.MakingTour
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.people_here.Data.MakingTourAddListData
 import com.example.people_here.R
 import com.example.people_here.databinding.FragmentMakingTourAddListBinding
+import java.util.Collections
 
 class MakingTourAddListFragment : Fragment() {
     lateinit var binding : FragmentMakingTourAddListBinding
     private var addListAdapter : MakingTourAddListAdapter?= null
     private var addListData : ArrayList<MakingTourAddListData> = arrayListOf()
+    private var isEditMode : Boolean = false
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,21 +33,108 @@ class MakingTourAddListFragment : Fragment() {
         initRecyclerView()
         //API 통신 시 데이터 수정
         initDummyData()
+        setupItemTouchHelper()
 
         binding.btnMakingTourAddListEditSequence.setOnClickListener{
             initEditSequence()
+        }
 
+        binding.btnMakingTourAddListFinishSequence.setOnClickListener {
+            initEditSequence()
         }
 
         return binding.root
     }
 
-    private fun initEditSequence() {
-        binding.btnMakingTourAddListEditSequence.isGone = true
-        binding.btnMakingTourAddListFinishSequence.isVisible = true
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (isEditMode) {
+                    // 슌서 편집 시 다이얼로그
+                    showExitEditDialog()
+                } else {
+                    // 일반 뒤로가기
+                    isEnabled = false
+                    requireActivity().onBackPressed()
+                }
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
     }
+
+    private fun showExitEditDialog() {
+        AlertDialog.Builder(requireContext()).apply {
+            setTitle("편집 종료")
+            setMessage("순서 편집을 종료하시겠습니까?")
+            setPositiveButton("예") { dialog, which ->
+                isEditMode = false
+                // 필요한 경우 다른 UI 업데이트 로직 추가
+                dialog.dismiss()
+            }
+            setNegativeButton("아니오") { dialog, which ->
+                dialog.dismiss()
+            }
+            show()
+        }
+    }
+
+
+    private fun initEditSequence() {
+        isEditMode = !isEditMode
+        addListAdapter?.setEditMode(isEditMode)
+
+        if (isEditMode){
+            binding.btnMakingTourAddListEditSequence.isGone = true
+            binding.btnMakingTourAddListFinishSequence.isVisible = true
+        }else{
+            binding.btnMakingTourAddListEditSequence.isVisible = true
+            binding.btnMakingTourAddListFinishSequence.isGone = true
+        }
+    }
+
+    private fun setupItemTouchHelper() {
+        val itemTouchHelperCallback = object : ItemTouchHelper.Callback() {
+            override fun isLongPressDragEnabled(): Boolean {
+                return isEditMode  // 에디트 모드일 때만 드래그 가능
+            }
+
+            override fun getMovementFlags(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ): Int {
+                val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
+                return makeMovementFlags(dragFlags, 0)
+            }
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                val fromPosition = viewHolder.adapterPosition
+                val toPosition = target.adapterPosition
+
+                if (toPosition == addListData.size - 1) {
+                    return false
+                }
+
+                addListAdapter?.onItemMove(fromPosition, toPosition)
+                return true
+
+            }
+
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                // 추후 swipe 기능 추가 되면 추가
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
+        itemTouchHelper.attachToRecyclerView(binding.rvMakingTourAddListPlace)
+    }
+
 
     private fun initDummyData() {
         addListData.addAll(

1f30e7ec8327e69e2f9382c0dc85549b18d37b0f
TaejungKim <tae0803@naver.com>
2024-01-05 13:18:30 +0900

    임의 검색어 api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,8 @@ dependencies {
     //지도 api
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
+
+    //검색어 api
+    implementation ("com.google.android.libraries.places:places:3.3.0")
+
 }

1b5c5983d235062c4f483ef261451a4cac388c2b
cheerjihyeon <gimjihyeon111@gmail.com>
2024-01-19 11:33:39 +0900

    240119_commit

--- app/src/main/java/com/example/people_here/MyTour/MyTourFragment.kt
+++ app/src/main/java/com/example/people_here/MyTour/MyTourFragment.kt
@@ -1,11 +1,13 @@
 package com.example.people_here.MyTour
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.people_here.Search.MainSearchActivity
 import com.example.people_here.databinding.FragmentMyTourBinding
 
 class MyTourFragment : Fragment() {
@@ -16,9 +18,15 @@ class MyTourFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.d("test","test")
         binding = FragmentMyTourBinding.inflate(layoutInflater)
-        
+
+        //장소 검색 페이지로 넘어가기 구현
+        binding.btnAddPlace.setOnClickListener {
+            val intent = Intent(requireContext(), MainSearchActivity::class.java)
+            startActivity(intent)
+        }
+
+
         return binding.root
     }
 

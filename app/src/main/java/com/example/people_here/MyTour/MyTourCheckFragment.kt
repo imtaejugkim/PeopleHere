efaa0d6274b73b5216eb8c1b71d23b4612794175
cheerjihyeon <gimjihyeon111@gmail.com>
2024-01-19 12:24:44 +0900

    240119_commit_12:24

--- app/src/main/java/com/example/people_here/MyTour/MyTourCheckFragment.kt
+++ app/src/main/java/com/example/people_here/MyTour/MyTourCheckFragment.kt
@@ -1,4 +1,18 @@
 package com.example.people_here.MyTour
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.people_here.Search.MainSearchActivity
+import com.example.people_here.databinding.FragmentMyTourBinding
+import com.example.people_here.databinding.FragmentMyTourCheckBinding
+
 class MyTourCheckFragment {
+
+
+    private lateinit var binding : FragmentMyTourCheckBinding
+
+
 }

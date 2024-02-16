c819cbeb373856a277d152cbf8a6beeb604d946f
jungan777 <jungan0403@gmail.com>
2024-02-17 01:45:52 +0900

    2024-02-17 commit_02

--- app/src/main/java/com/peopleHere/people_here/MyTour/MakingCourseSearchActivity.kt
+++ app/src/main/java/com/peopleHere/people_here/MyTour/MakingCourseSearchActivity.kt
@@ -24,39 +24,5 @@ class MakingCourseSearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMakingCourseSearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        if (!Places.isInitialized()) {
-            Places.initialize(applicationContext, "YOUR_API_KEY")
-        }
-
-        placesClient = Places.createClient(this)
-
-        val token = AutocompleteSessionToken.newInstance()
-        val bounds = RectangularBounds.newInstance(
-            LatLng(-33.880490, 151.184363), // Southwest corner
-            LatLng(-33.858754, 151.229596) // Northeast corner
-        )
-
-        val autocompleteFragment = supportFragmentManager.findFragmentById(R.id.search_fragment) as AutocompleteSupportFragment
-        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG))
-
-        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
-            override fun onPlaceSelected(place: Place) {
-                // Handle the selected place
-                place.latLng?.let { latLng ->
-                    binding.dummyText.text = latLng.toString()
-                    Toast.makeText(this@MakingCourseSearchActivity, latLng.toString(), Toast.LENGTH_SHORT).show()
-                }
-            }
-
-            override fun onError(status: Status) {
-                Log.e("PlacesError", status.statusMessage ?: "")
-            }
-        })
-
-        // Configure the AutocompleteSupportFragment to use the bounds and session token
-        autocompleteFragment.setCountries("AU", "NZ")
-        autocompleteFragment.setLocationBias(bounds)
-        // Note: setLocationRestriction(bounds) can be used instead of setLocationBias(bounds) depending on the desired behavior
-        autocompleteFragment.setSessionToken(token)
-    }}
+}
+}

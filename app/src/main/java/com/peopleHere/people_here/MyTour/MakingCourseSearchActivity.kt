515ce4172c2e119df6be6733f27581d307e39295
cheerjihyeon <gimjihyeon111@gmail.com>
2024-02-16 20:30:16 +0900

    240216_20:30 commit

--- app/src/main/java/com/peopleHere/people_here/MyTour/MakingCourseSearchActivity.kt
+++ app/src/main/java/com/peopleHere/people_here/MyTour/MakingCourseSearchActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.common.api.Status
+import com.google.android.gms.common.api.ApiException
 import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.AutocompleteSessionToken
 import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.RectangularBounds
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
+import com.google.android.libraries.places.api.net.PlacesClient
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 import com.peopleHere.people_here.R
@@ -14,6 +18,7 @@ import com.peopleHere.people_here.databinding.ActivityMakingCourseSearchBinding
 
 class MakingCourseSearchActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMakingCourseSearchBinding
+    private lateinit var placesClient: PlacesClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +29,20 @@ class MakingCourseSearchActivity : AppCompatActivity() {
             Places.initialize(applicationContext, "YOUR_API_KEY")
         }
 
-        val autocompleteFragment = (supportFragmentManager.findFragmentById(R.id.search_fragment) as AutocompleteSupportFragment)
-            .setPlaceFields(listOf(Place.Field.LAT_LNG, Place.Field.NAME))
+        placesClient = Places.createClient(this)
+
+        val token = AutocompleteSessionToken.newInstance()
+        val bounds = RectangularBounds.newInstance(
+            LatLng(-33.880490, 151.184363), // Southwest corner
+            LatLng(-33.858754, 151.229596) // Northeast corner
+        )
+
+        val autocompleteFragment = supportFragmentManager.findFragmentById(R.id.search_fragment) as AutocompleteSupportFragment
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG))
 
         autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
             override fun onPlaceSelected(place: Place) {
+                // Handle the selected place
                 place.latLng?.let { latLng ->
                     binding.dummyText.text = latLng.toString()
                     Toast.makeText(this@MakingCourseSearchActivity, latLng.toString(), Toast.LENGTH_SHORT).show()
@@ -39,5 +53,10 @@ class MakingCourseSearchActivity : AppCompatActivity() {
                 Log.e("PlacesError", status.statusMessage ?: "")
             }
         })
+
+        // Configure the AutocompleteSupportFragment to use the bounds and session token
+        autocompleteFragment.setCountries("AU", "NZ")
+        autocompleteFragment.setLocationBias(bounds)
+        // Note: setLocationRestriction(bounds) can be used instead of setLocationBias(bounds) depending on the desired behavior
+        autocompleteFragment.setSessionToken(token)
     }
-}
